148b18968f5acd95e16cf8ee4ae4aad41b964cb3
Alessandro Dicosola <alessandro.dicosola@outlook.it>
2020-11-06 10:31:00 +0100

    Inserted type hints and get_optimizers method

--- src/base/BaseModel.py
+++ src/base/BaseModel.py
@@ -1,23 +1,31 @@
 import torch
 import torch.nn as nn
 import utility
-from abc import ABC,abstractmethod
-from pathlib import Path
 
+from typing import List,Tuple,Mapping,Union
+
+from abc import ABC, abstractmethod
+from pathlib import Path
 
 class BaseModel(nn.Module, ABC):
     """
     Base class for handling neural networks
     """
+
     def __init__(self):
-        super(BaseModel,self).__init__()
-        #e.g. BaseModel
-        self.name : str = self.__class__.__name__
-        #e.g. ./models/BaseModels
-        self.model_dir : Path = utility.get_models_dir() / self.name
+        super(BaseModel, self).__init__()
+
+        # e.g. BaseModel
+        self.name: str = self.__class__.__name__
+        # e.g. ./models/BaseModels
+        self.model_dir: Path = utility.get_models_dir() / self.name
+
+        # retrieve the optimizer
+        self.optimizer : torch.optim.Optimizer = self.get_optimizer()
+
 
     @abstractmethod
-    def forward(self,input):
+    def forward(self, input : torch.Tensor) -> torch.Tensor:
         """
         Base method for inference phase
         :param input: input
@@ -27,14 +35,13 @@ class BaseModel(nn.Module, ABC):
         """
         raise NotImplementedError("forward")
 
-
     @abstractmethod
-    def train_step(self,batch,batch_index):
+    def train_step(self, batch : Union[Tuple[torch.Tensor,...],torch.Tensor], batch_index: int):
         """
         Implement training step called by [BaseTrainer]
         :param batch: batch
         :param batch_index: batch index
-        :return: training information
+        :return: loss (for doing loss.backwards()) and other information
 
         ATTENTION: must call super().train_step(batch,batch_index) otherwise unexpected result will be seen during training
         """
@@ -46,18 +53,31 @@ class BaseModel(nn.Module, ABC):
 
     @abstractmethod
     @torch.no_grad()
-    def val_step(self,batch,batch_index):
+    def val_step(self, batch : Union[Tuple[torch.Tensor,...],torch.Tensor], batch_index : int):
         """
         Implement validation step called by [BaseTrainer]
         :param batch: batch
         :param batch_index: batch index
-        :return: training information
+        :return: :return: loss and other information
 
         NOTE: @torch.no_grad() avoid to compute gradients.
         ATTENTION: must call super().val_step(batch,batch_index) otherwise unexpected result will be seen during validation
         """
 
-        #set the model in validation phase
+        # set the model in validation phase
         self.eval()
 
         pass
+
+    @abstractmethod
+    def get_optimizers(self) -> Union[torch.optim.Optimizer,List[torch.optim.Optimizer],Mapping[str,torch.optim.Optimizer],Tuple[torch.optim.Optimizer,...]]:
+        """
+        Function for retrieving the optimizer(s) necessary during the train_step and val_step
+        :param optimizer: to
+        :param kwargs:
+        :return: optimizer or list of optimizer or dict of optimizer
+        """
+        raise NotImplementedError("get_optimizer")
+
+
+
