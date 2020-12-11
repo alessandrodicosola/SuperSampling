30b6dbd618d1275f06954add0f841bedfff759ff
Alessandro Dicosola <alessandro.dicosola@outlook.it>
2020-12-11 10:37:34 +0100

    Refactor Trainer.py and associated test

--- src/base/Trainer.py
+++ src/base/Trainer.py
@@ -6,7 +6,7 @@ import torch.utils.tensorboard
 from base import BaseModel
 from base.Callback import Callback, ListCallback
 
-from base.hints import Union, List, Tuple
+from base.hints import Union, List, Tuple, Criterion
 
 from tqdm.auto import tqdm
 
@@ -14,6 +14,8 @@ import sys
 import logging
 
 logging.basicConfig()
+logger = logging.getLogger("Trainer")
+logger.setLevel(logging.DEBUG)
 
 
 class Trainer:
@@ -31,21 +33,27 @@ class Trainer:
                  experiment: str,
                  model: BaseModel,
                  optimizer: torch.optim.Optimizer,
-                 criterion,
+                 criterion: Criterion,
                  lr_scheduler: torch.optim.lr_scheduler = None,
                  callback: Union[Callback, ListCallback] = None,
-                 device : torch.device = None):
+                 device: torch.device = None):
+        # set the experiment name
+        self.experiment = experiment
 
         if device is None:
             device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+            # if device is None model and criterion are not in the same device
+            self.model = model.to(device)
+            self.criterion = criterion.to(device)
+        else:
+            self.model = model
+            self.criterion = criterion
+
+        # set the device for moving batches
         self.device = device
-
-        self.experiment = experiment
-
-        self.model = model
         self.optimizer = optimizer
         self.lr_scheduler = lr_scheduler
-        self.criterion = criterion
+
         self.callback = callback
 
         # flag used for stopping the training
@@ -65,78 +73,104 @@ class Trainer:
         # self.logger.addHandler(fileHandler)
 
         # self.logger.info(f"Initialized Trainer for {experiment} with {model.name}")
+        logger.debug(f"Init trainer class for {experiment} with {model.name}")
 
-    def stop(self):
+    def stop(self, sender=None):
+        logger.debug(f"Trainer stopped by {'itself' if sender is None else sender.__class__.__name__}")
         self.stop = True
 
     def train(self, loader: torch.utils.data.DataLoader):
         """Define the train loop for one epoch"""
 
+        logger.debug("Set model in training mode")
         self.model.train()
 
         epoch_loss = 0
+        total_samples = 0
 
         for index_batch, (X, y) in enumerate(tqdm(loader, total=len(loader), unit="batch", leave=False)):
-            #extract batch size from X
-            BATCH_SIZE = self._get_batch_size(X)
+            logger.debug(f"Input received: {type(X)}")
+            logger.debug(f"Target received: {type(y)}")
 
-            # move batch and targets to device
-            X, y = self._convert_to_device(X, y)
-
+            logger.debug("Erasing gradients inside the optimizer")
             # erase all saved gradients
             self.optimizer.zero_grad()
+
+            # extract batch size from X
+            logger.debug("Extracting batch size")
+            BATCH_SIZE = self._get_batch_size(X)
+            total_samples += BATCH_SIZE
+            logger.debug(f"Batch size: {BATCH_SIZE}")
+
+            # move batch and targets to device
+            logger.debug("Move X and y to the selected device")
+            X, y = self._convert_to_device(X, y)
 
             # forward the input in the correct way:
             # e.g:
             # forward(input)
             # forward(input1,input2,input3) we have to unfold the list or the tuple
+            logger.debug("Forwarding the input")
             if isinstance(X, torch.Tensor):
                 out = self.model.train_step(X)
             elif isinstance(X, (List, Tuple)):
                 out = self.model.train_step(*X)
             else:
                 raise RuntimeError(f"Invalid type: {type(X)}")
+            self._assert_out_requires_grad(out, train=True)
 
             # compute the loss
+            logger.debug("Computing the loss")
+            assert out.size() == y.size()
             loss = self.criterion(out, y)
+
             # compute the gradients
+            logger.debug("Computing the gradients")
             loss.backward()
+
             # update the parameters
+            logger.debug("Updating the parameters")
             self.optimizer.step()
 
             # update the loss
             # loss.item() contains the loss averaged in the batch
             # * X.size(0) we get the non averaged loss in the batch
+            logger.debug("Updating the epoch loss counter")
             epoch_loss += loss.item() * BATCH_SIZE
 
         # return the averaged loss in the whole dataset
-        return epoch_loss / len(loader)
+        logger.debug("Returning loss of the whole dataset")
+        return epoch_loss / total_samples
 
     def validation(self, loader: torch.utils.data.DataLoader):
         self.model.eval()
 
         epoch_loss = 0
+        total_samples = 0
 
-        with torch.no_grad():
-            for index_batch, (X, y) in enumerate(tqdm(loader, total=len(loader), unit="batch", leave=False)):
-                BATCH_SIZE = self._get_batch_size(X)
 
-                X, y = self._convert_to_device(X,y)
+        for index_batch, (X, y) in enumerate(tqdm(loader, total=len(loader), unit="batch", leave=False)):
+            BATCH_SIZE = self._get_batch_size(X)
+            total_samples += BATCH_SIZE
 
-                if isinstance(X, torch.Tensor):
-                    out = self.model.val_step(X)
-                elif isinstance(X, (List, Tuple)):
-                    out = self.model.val_step(*X)
-                else:
-                    raise RuntimeError(f"Invalid type: {type(X)}")
+            X, y = self._convert_to_device(X, y)
 
-                loss = self.criterion(out, y)
+            if isinstance(X, torch.Tensor):
+                out = self.model.val_step(X)
+            elif isinstance(X, (List, Tuple)):
+                out = self.model.val_step(*X)
+            else:
+                raise RuntimeError(f"Invalid type: {type(X)}")
+            self._assert_out_requires_grad(out, train=False)
 
-                epoch_loss += loss.item() * BATCH_SIZE
+            assert out.size() == y.size()
+            loss = self.criterion(out, y)
 
-        return epoch_loss / len(loader)
+            epoch_loss += loss.item() * BATCH_SIZE
 
-    def fit(self, train_loader : torch.utils.data.DataLoader, val_loader : torch.utils.data.DataLoader, epochs: int):
+        return epoch_loss / total_samples
+
+    def fit(self, train_loader: torch.utils.data.DataLoader, val_loader: torch.utils.data.DataLoader, epochs: int):
 
         for epoch in range(1, epochs + 1):
 
@@ -145,10 +179,10 @@ class Trainer:
                 break
 
             if self.callback:
-                self.callback.start_epoch(self._create_args_for_callback(epoch = epoch))
+                self.callback.start_epoch(self._create_args_for_callback(epoch=epoch))
 
             train_loss = self.train(train_loader)
-            val_loss   = self.validation(val_loader)
+            val_loss = self.validation(val_loader)
 
             print(f"Epoch: {epoch} Train Loss: {train_loss} Val Loss: {val_loss}")
 
@@ -156,20 +190,27 @@ class Trainer:
                 self.lr_scheduler.step()
 
             if self.callback:
-                self.callback.end_epoch(self._create_args_for_callback(train_loss = train_loss, val_loss = val_loss, epoch = epoch))
+                self.callback.end_epoch(
+                    self._create_args_for_callback(train_loss=train_loss, val_loss=val_loss, epoch=epoch))
 
+    def _assert_out_requires_grad(self, out, train: bool):
+        if isinstance(out, torch.Tensor):
+            assert out.requires_grad == train
+        elif isinstance(out, (List, Tuple)):
+            for elem in out:
+                if isinstance(elem, torch.Tensor):
+                    assert elem.requires_grad == train
 
-    def _get_batch_size(self,X):
+    def _get_batch_size(self, X):
         if isinstance(X, torch.Tensor):
             return X.size(0)
-        elif isinstance(X, (List,Tuple)):
-            sizes = [elem.size(0) for elem in X if isinstance(elem,torch.Tensor)]
+        elif isinstance(X, (List, Tuple)):
+            sizes = [elem.size(0) for elem in X if isinstance(elem, torch.Tensor)]
             return sizes[0]
         else:
             raise RuntimeError(f"Invalid type: {type(X)}")
 
-
-    def _convert_to_device(self,X,y):
+    def _convert_to_device(self, X, y):
         if isinstance(X, torch.Tensor):
             X = X.to(self.device)
         elif isinstance(X, Tuple):
@@ -188,8 +229,7 @@ class Trainer:
         else:
             raise RuntimeError(f"Invalid type: {type(y)}")
 
-        return X,y
-
+        return X, y
 
     def _create_args_for_callback(self, **kwargs):
         """ Return a dictionary of arguments to pass to callbacks
@@ -206,8 +246,8 @@ class Trainer:
         """
 
         base_kwargs = {
-            "experiment" : self.experiment,
-            "model" : self.model,
+            "experiment": self.experiment,
+            "model": self.model,
             "optimizer": self.optimizer,
         }
 
--- tests/test_Trainer.py
+++ tests/test_Trainer.py
@@ -4,33 +4,124 @@ from unittest import TestCase
 import torch
 from torch.nn import MSELoss
 from torch.optim import Adam
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
 
+from base import BaseModel
 from base.Trainer import Trainer
 from datasets.ASDNDataset import ASDNDataset, collate_fn
 from models.ASDN import ASDN
 from models.LaplacianFrequencyRepresentation import LaplacianFrequencyRepresentation
 
 
+class RandomDataset(Dataset):
+    def __init__(self, n_input, input_size, target_size, size_dataset=8):
+        self.size_dataset = size_dataset
+        self.n_input = n_input
+        self.input_size = input_size
+        self.target_size = target_size
+
+    def __len__(self):
+        return self.size_dataset
+
+    def __getitem__(self, item):
+        # (X,y)
+        return tuple([torch.rand(self.input_size) for _ in range(self.n_input)]), torch.rand(self.target_size)
+
+
+class NetworkOneInput(BaseModel):
+
+    def train_step(self, input):
+        return self(input)
+
+    @torch.no_grad()
+    def val_step(self, input):
+        return self(input)
+
+    @torch.no_grad()
+    def test_step(self, input):
+        return self(input)
+
+    def __init__(self, input_size, low_features):
+        super(NetworkOneInput, self).__init__()
+        self.conv = torch.nn.Conv2d(in_channels=input_size[0], out_channels=low_features, kernel_size=3, padding=1)
+        self.out = torch.nn.Conv2d(in_channels=low_features, out_channels=input_size[0], kernel_size=3, padding=1)
+
+    def forward(self, input) -> torch.Tensor:
+        return self.out(self.conv(input))
+
+
+class NetworkTwoInputs(BaseModel):
+
+    def __init__(self, input_size, low_features):
+        super(NetworkTwoInputs, self).__init__()
+        self.conv = torch.nn.Conv2d(in_channels=input_size[0], out_channels=low_features, kernel_size=3, padding=1)
+        self.out = torch.nn.Conv2d(in_channels=low_features, out_channels=input_size[0], kernel_size=3, padding=1)
+
+    def forward(self, input1, input2) -> torch.Tensor:
+        out1 = self.conv(input1)
+        out2 = self.conv(input2)
+        out = out1 + out2
+        return self.out(out)
+
+    def train_step(self, input1, input2):
+        return self(input1, input2)
+
+    @torch.no_grad()
+    def val_step(self, input1, input2):
+        return self(input1, input2)
+
+    @torch.no_grad()
+    def test_step(self, input1, input2):
+        return self(input1, input2)
+
+
 class TestTrainer(TestCase):
 
-    def test_fit(self):
-
-        lfr = LaplacianFrequencyRepresentation(1,2,11)
-        collate_fn_lfr = partial(collate_fn,lfr = lfr)
+    @unittest.skip("EXPENSIVE")
+    def test_fit_with_ASDN(self):
+        lfr = LaplacianFrequencyRepresentation(1, 2, 11)
+        collate_fn_lfr = partial(collate_fn, lfr=lfr)
 
         val_dataset = ASDNDataset("DIV2K_valid_HR", 24, lfr)
         val_loader = DataLoader(val_dataset, 64, False, num_workers=4, collate_fn=collate_fn_lfr)
-        train_dataset = ASDNDataset("DIV2K_train_HR", 24,lfr)
-        train_loader  = DataLoader(train_dataset,32,True,num_workers=4,collate_fn=collate_fn_lfr)
-
+        train_dataset = ASDNDataset("DIV2K_train_HR", 24, lfr)
+        train_loader = DataLoader(train_dataset, 32, True, num_workers=4, collate_fn=collate_fn_lfr)
 
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        asdn = ASDN(3,lfr,n_dab=3,n_intra_layers=3,out_compressed_channels=32,out_channels_dab=8,intra_layer_output_features=8).to(device)
+        asdn = ASDN(3, lfr, n_dab=3, n_intra_layers=3, out_compressed_channels=32, out_channels_dab=8,
+                    intra_layer_output_features=8).to(device)
 
-        adam = Adam(asdn.parameters(),1e-3,betas=(0.99,0.999),eps=1e-8)
-        trainer = Trainer("test",asdn,adam,MSELoss().to(device),device=device)
-        trainer.fit(train_loader,val_loader,1)
+        adam = Adam(asdn.parameters(), 1e-3, betas=(0.99, 0.999), eps=1e-8)
+        trainer = Trainer("test", asdn, adam, MSELoss().to(device), device=device)
+        trainer.fit(train_loader, val_loader, 1)
+
+    def test_fit_one_input(self):
+        device = torch.device('cpu')
+        input_size = (3, 24, 24)
+        dataset = RandomDataset(1, input_size, input_size)
+        dataloader = DataLoader(dataset, batch_size=32)
+        loss = MSELoss().to(device)
+        model = NetworkOneInput(input_size, 32).to(device)
+        adam = Adam(model.parameters(), 1e-3, betas=(0.99, 0.999), eps=1e-8)
+        trainer = Trainer("test", model, adam, loss, device=device)
+        trainer.fit(dataloader, dataloader, 5)
+
+    def test_fit_two_inputs(self):
+        def collate_fn(batch):
+            inputs, target = zip(*batch)
+            input1, input2 = zip(*inputs)
+            return (torch.stack(input1, dim=0), torch.stack(input2, dim=0)), torch.stack(target, dim=0)
+
+        device = torch.device('cpu')
+        input_size = (3, 24, 24)
+        dataset = RandomDataset(2, input_size, input_size)
+        dataloader = DataLoader(dataset, batch_size=32, collate_fn=collate_fn)
+        loss = MSELoss().to(device)
+        model = NetworkTwoInputs(input_size, 32).to(device)
+        adam = Adam(model.parameters(), 1e-3, betas=(0.99, 0.999), eps=1e-8)
+        trainer = Trainer("test", model, adam, loss, device=device)
+        trainer.fit(dataloader, dataloader, 5)
+
 
 if __name__ == "__main__":
     unittest.main()
