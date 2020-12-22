bfe63ed98a4ebc8748e0315501b0b589adb1ca22
Alessandro Dicosola <alessandro.dicosola@outlook.it>
2020-12-22 12:33:53 +0100

    - Refactored PSNR inserting reduction

--- src/base/metrics/PSNR.py
+++ src/base/metrics/PSNR.py
@@ -10,12 +10,22 @@ class PSNR(torch.nn.Module):
 
         PSNR = 10 \cdot \log_10 \\frac{MAX_I^2}{\sqrt{MSE_{P,T}}}
 
+    Args:
+        max_pixel_value: 1 if grayscale, 3 if rgb
+        reduction: Reduction applied over the batch: none (not supported by Trainer) | mean | sum
     """
 
-    def __init__(self, max_pixel_value=255):
+    def __init__(self, max_pixel_value: float = 255., reduction=None):
+        """
+
+        """
         super(PSNR, self).__init__()
         self.max_pixel_value = max_pixel_value
         self.eps = 1e-8
+        self.reduction = reduction
+
+        if not reduction:
+            raise RuntimeError("Trainer supports only metrics that returns float")
 
     @torch.no_grad()
     def forward(self, prediction: torch.Tensor, target: torch.Tensor):
@@ -27,9 +37,23 @@ class PSNR(torch.nn.Module):
         prediction = prediction / self.max_pixel_value
         target = target / self.max_pixel_value
 
-        mse = ((prediction - target) ** 2).view(-1, 1).mean(dim=1).sum()
-        if mse == 0:
-            return 100
+        rank = len(prediction.size())
 
-        psnr = 10 * torch.log10(1 / (mse + self.eps))
-        return psnr.item()
+        # (C,H,W) if r=3 else (B,C,H,W)
+        width_dim = rank - 1
+        height_dim = width_dim - 1
+        channel_dim = height_dim - 1
+        batch_dim = channel_dim - 1
+
+        mse = torch.mean((prediction - target) ** 2, dim=[channel_dim, height_dim, width_dim])
+        mse = mse + self.eps
+        # if mse is close to 0 returns 100 otherwise returns psnr
+        psnr = torch.where(torch.isclose(mse, torch.zeros_like(mse)), 100 * torch.ones_like(mse),
+                           - 10 * torch.log10(mse))
+
+        if self.reduction == "mean":
+            return psnr.mean(dim=batch_dim).item()
+        elif self.reduction == "sum":
+            return psnr.sum(dim=batch_dim).item()
+        else:
+            return psnr
