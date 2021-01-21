a61fda0801e7edc8aa90855c198006ed3ae49689
Alessandro Dicosola <alessandro.dicosola@outlook.it>
2021-01-21 17:58:35 +0100

    Implemented ability to read n files and using n name. Values are smoothed.

--- src/handle_csv.py
+++ src/handle_csv.py
@@ -1,29 +1,43 @@
+from typing import List
+
 import pandas as pd
 import argparse
 from pathlib import Path
 from functools import partial
+import matplotlib.pyplot as plt
 
+def prepare_csv(files: List[Path], names: List[str]):
+    def iterate(files, names):
+        for file, name in zip(files, names):
+            df = pd.read_csv(file, index_col="Step", parse_dates=['Wall time'],
+                             date_parser=partial(pd.to_datetime, unit='s'))
+            df.rename(inplace=True, columns={
+                "Step": "step",
+                "Wall time": "wall_time",
+                "Value": "value"
+            })
+            yield df.add_suffix(f"_{name}")
 
-def prepare_csv(filepath):
-    df = pd.read_csv(filepath, index_col="Step", parse_dates=['Wall time'],
-                     date_parser=partial(pd.to_datetime, unit='s'))
-    df.rename(inplace=True, columns={
-        "Step": "step",
-        "Wall time": "wall time",
-        "Value": "value"
-    })
-    return df
+    return pd.concat(iterate(files, names), axis=1)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("handle_csv.py")
-    parser.add_argument("filepath", type=str)
+    parser.add_argument("--files", required=True, type=str, nargs="+")
+    parser.add_argument("--names", required=True, type=str, nargs="+")
     result = parser.parse_args()
-    file = Path(result.filepath)
 
-    if not file.exists():
-        raise FileNotFoundError(str(file))
+    files = [Path(file) for file in result.files]
 
-    df = prepare_csv(file)
+    for file in files:
+        if not file.exists():
+            raise FileNotFoundError(str(file))
 
-    df.plot.line()
+    names = result.names
+
+    df = prepare_csv(files=files, names=names)
+
+    cols = [f"value_{name}" for name in names]
+
+    df[cols].ewm(alpha=0.1).mean().plot.line()
+    plt.show()
