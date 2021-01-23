4e8064af5de5458029245654f4dba1149251a683
Alessandro Dicosola <alessandro.dicosola@outlook.it>
2021-01-23 08:46:18 +0100

    modified handle_csv

--- src/handle_csv.py
+++ src/handle_csv.py
@@ -23,6 +23,7 @@ def prepare_csv(files: List[Path], names: List[str]):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser("handle_csv.py")
+    parser.add_argument("--alpha", required=False, type=float, default=0.3)
     parser.add_argument("--files", required=True, type=str, nargs="+")
     parser.add_argument("--names", required=True, type=str, nargs="+")
     result = parser.parse_args()
@@ -39,5 +40,7 @@ if __name__ == '__main__':
 
     cols = [f"value_{name}" for name in names]
 
-    df[cols].ewm(alpha=0.1).mean().plot.line()
+    df[cols].ewm(alpha=result.alpha).mean().plot.line(ylim=(0.00001, 0.1))
+
+
     plt.show()
